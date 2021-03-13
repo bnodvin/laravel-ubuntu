618cbc8298cf963a2970a15c3870e2215231d525
bnodvin <benjamin@nodvin.com>
2021-03-13 14:29:32 -0500

    commit on main

--- resources/js/Pages/Resources/Index.vue
+++ resources/js/Pages/Resources/Index.vue
@@ -8,18 +8,7 @@
 
         <div>
             <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-                <update-team-name-form :team="team" :permissions="permissions" />
-
-                <team-member-manager class="mt-10 sm:mt-0"
-                            :team="team"
-                            :available-roles="availableRoles"
-                            :user-permissions="permissions" />
-
-                <template v-if="permissions.canDeleteTeam && ! team.personal_team">
-                    <jet-section-border />
-
-                    <delete-team-form class="mt-10 sm:mt-0" :team="team" />
-                </template>
+             ok
             </div>
         </div>
     </app-layout>
