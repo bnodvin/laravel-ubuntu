cbba59dbe63dc7667a5548e5208fe7ed10dd0043
bnodvin <benjamin@nodvin.com>
2021-03-13 14:21:20 -0500

    commit on main

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -19,7 +19,10 @@
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <jet-nav-link :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
-                                </jet-nav-link>=
+                                </jet-nav-link>
+                                <jet-nav-link :href="route('resources')" :active="route().current('resources')">
+                                    Resources
+                                </jet-nav-link>
                             </div>
                         </div>
 
